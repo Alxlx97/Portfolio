abb5f66c1c6e4a4896f8d5a3b2373b1fc8bfaf7c
Alxlx97 <Alexandre1997Lemieux@outlook.com>
2026-03-29 16:39:56 -0400

    Small fix in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,6 @@ export default function Footer() {
                                       </svg>
                                     </span>
                         </a>
-
                     </div>
                 </div>
             </div>
